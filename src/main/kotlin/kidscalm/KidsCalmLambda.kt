bfc37b36420c274893991d42d63079d2b05b3502
Jason Fehr <jasonmfehr@gmail.com>
2017-06-21 22:05:47 -0500

    added KidsCalm Alexa skill id to list of supported apps

--- src/main/kotlin/kidscalm/KidsCalmLambda.kt
+++ src/main/kotlin/kidscalm/KidsCalmLambda.kt
@@ -3,11 +3,8 @@ package kidscalm
 import com.amazon.speech.speechlet.Speechlet
 import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler
 
-val supportedApps = HashSet<String>()
-
-
 class KidsCalmLambda : SpeechletRequestStreamHandler {
 
-    constructor() : super(KidsCalmSpeechlet(), supportedApps)
+    constructor() : super(KidsCalmSpeechlet(), setOf("amzn1.ask.skill.73c3be5b-911d-41ea-9c62-65854bbdc2b1"))
 
 }
