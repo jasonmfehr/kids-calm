739434e5020da717b431ea24fe7f0d88158b5bc5
Jason Fehr <jasonmfehr@gmail.com>
2017-06-24 14:16:25 -0500

    added list of responses and unit tests

--- src/main/kotlin/kidscalm/KidsCalmSpeechlet.kt
+++ src/main/kotlin/kidscalm/KidsCalmSpeechlet.kt
@@ -2,15 +2,30 @@ package kidscalm
 
 import com.amazon.speech.json.SpeechletRequestEnvelope
 import com.amazon.speech.speechlet.*
+import com.amazon.speech.ui.OutputSpeech
 import com.amazon.speech.ui.PlainTextOutputSpeech
 import com.amazon.speech.ui.Reprompt
 import org.slf4j.LoggerFactory
+import java.util.*
 
 class KidsCalmSpeechlet : SpeechletV2 {
+
     private val LOGGER = LoggerFactory.getLogger("kidscalm.KidsCalmSpeechlet")
+
+    private val RESPONSES_SENT_KEY = "responsesSent"
+
+    internal val responses = listOf(
+            "Do ten jumping jacks",
+            "Do yoga child's pose",
+            "Do yoga downward dog",
+            "Take 3 slow deep breaths",
+            "Hugs and kisses",
+            "Do a short backrub"
+    )
 
     override fun onSessionStarted(requestEnvelope: SpeechletRequestEnvelope<SessionStartedRequest>?) {
         LOGGER.info("onSessionStarted USER_ID=\"${requestEnvelope?.session?.user?.userId}\" SESSION_ID=\"${requestEnvelope?.session?.sessionId}\"")
+        requestEnvelope?.session?.attributes?.put(RESPONSES_SENT_KEY, mutableListOf<Int>())
     }
 
     override fun onSessionEnded(requestEnvelope: SpeechletRequestEnvelope<SessionEndedRequest>?) {
@@ -21,23 +36,47 @@ class KidsCalmSpeechlet : SpeechletV2 {
         LOGGER.info("onIntent INTENT_NAME=\"${requestEnvelope?.request?.intent?.name}\" USER_ID=\"${requestEnvelope?.session?.user?.userId}\" SESSION_ID=\"${requestEnvelope?.session?.sessionId}\"")
 
         val speech = PlainTextOutputSpeech()
-        val reprompt = Reprompt()
+        val responsesSent = requestEnvelope?.session?.getAttribute(RESPONSES_SENT_KEY) ?: mutableListOf<Int>()
 
-        speech.text = "Intent not yet implemented."
-        reprompt.outputSpeech = speech
+        speech.text = this.pickResponse(responsesSent as MutableList<Int>)
+        return SpeechletResponse.newAskResponse(speech, buildReprompt())
 
-        return SpeechletResponse.newAskResponse(speech, reprompt)
     }
 
     override fun onLaunch(requestEnvelope: SpeechletRequestEnvelope<LaunchRequest>?): SpeechletResponse {
         LOGGER.info("onLaunch USER_ID=\"${requestEnvelope?.session?.user?.userId}\" SESSION_ID=\"${requestEnvelope?.session?.sessionId}\"")
 
-        val speech = PlainTextOutputSpeech()
+        return SpeechletResponse.newAskResponse(buildOuptutSpeech("Welcome to Kids Calm beta."), buildReprompt())
+    }
+
+    private fun pickResponse(providedResponses: MutableList<Int>): String {
+        if(providedResponses.size == responses.size){
+            providedResponses.clear()
+        }
+
+        var respIndex: Int
+        do {
+            respIndex = Random().nextInt(responses.size)
+        } while(providedResponses.contains(respIndex))
+
+        providedResponses.add(respIndex)
+
+        return responses.get(respIndex)
+    }
+
+    private fun buildReprompt(): Reprompt {
         val reprompt = Reprompt()
 
-        speech.text = "Welcome to Kids Calm."
-        reprompt.outputSpeech = speech
+        reprompt.outputSpeech = buildOuptutSpeech("you can say things like suggestion, give me an idea, or tell me a game")
 
-        return SpeechletResponse.newAskResponse(speech, reprompt)
+        return reprompt
     }
+
+    private fun buildOuptutSpeech(text: String): OutputSpeech {
+        val speech = PlainTextOutputSpeech()
+        speech.text = text
+
+        return speech
+    }
+
 }
--- src/test/kotlin/kidscalm/KidsCalmSpeechletTest.kt
+++ src/test/kotlin/kidscalm/KidsCalmSpeechletTest.kt
@@ -1,19 +1,21 @@
 package kidscalm
 
-import com.amazon.speech.speechlet.SpeechletV2
-import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler
-import com.amazon.speech.ui.OutputSpeech
+import com.amazon.speech.json.SpeechletRequestEnvelope
+import com.amazon.speech.speechlet.IntentRequest
+import com.amazon.speech.speechlet.Session
+import com.amazon.speech.speechlet.SessionStartedRequest
+import com.amazon.speech.speechlet.User
 import com.amazon.speech.ui.PlainTextOutputSpeech
-import com.amazon.speech.ui.Reprompt
 import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.instanceOf
 import org.junit.Assert.assertThat
-import org.junit.Assert.fail
-import org.junit.Before
 import org.junit.Test
 
 class KidsCalmSpeechletTest {
 
-    var fixture = KidsCalmSpeechlet()
+    private val fixture = KidsCalmSpeechlet()
+    private val EXPECTED_ATTRIBUTE_KEY = "responsesSent"
+
 
     @Test
     fun testOnLaunchHappyPath() {
@@ -26,5 +28,27 @@ class KidsCalmSpeechletTest {
         assertThat(actualRepromptSpeech.text, `is`("Welcome to Kids Calm."))
     }
 
+    @Test
+    fun testOnIntentHappyPath() {
+        val session = Session.builder().withSessionId("foo").build()
+        val envelope = SpeechletRequestEnvelope.builder<IntentRequest>().withSession(session).build()
+        val sessionStarted = SpeechletRequestEnvelope.builder<SessionStartedRequest>().withSession(session).build()
+        val maxResponses = fixture.responses.size
+
+        fixture.onSessionStarted(sessionStarted)
+        assertThat(session.attributes.containsKey(EXPECTED_ATTRIBUTE_KEY), `is`(true))
+        assertThat(session.attributes.get(EXPECTED_ATTRIBUTE_KEY), instanceOf(MutableList::class.java))
+
+        for(i in 0..maxResponses*3) {
+            fixture.onIntent(envelope)
+            val responsesList = session.getAttribute(EXPECTED_ATTRIBUTE_KEY) as MutableList<Int>
+
+            if(i % maxResponses == 0){
+                assertThat(responsesList.size, `is`(1))
+            }else{
+                assertThat(responsesList.distinct().size, `is`(responsesList.size))
+            }
+        }
+    }
 
 }
