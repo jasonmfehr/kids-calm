94cfa679e59d5e11d90c2aace013bb1013c97ed3
Jason Fehr <jasonmfehr@gmail.com>
2017-06-27 07:11:27 -0500

    switching to new skill id

--- src/main/kotlin/kidscalm/KidsCalmLambda.kt
+++ src/main/kotlin/kidscalm/KidsCalmLambda.kt
@@ -5,6 +5,6 @@ import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler
 
 class KidsCalmLambda : SpeechletRequestStreamHandler {
 
-    constructor() : super(KidsCalmSpeechlet(), setOf("amzn1.ask.skill.73c3be5b-911d-41ea-9c62-65854bbdc2b1"))
+    constructor() : super(KidsCalmSpeechlet(), setOf("amzn1.ask.skill.44f9a5a7-4d87-4f0e-8a5d-bcd301b14e54"))
 
 }
